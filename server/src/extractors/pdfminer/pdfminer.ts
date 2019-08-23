bf7af7a6b3a133e01e613afe0898c6cb1d8df66b
Aarohi Johal <aarohi.johal@gmail.com>
2019-08-23 16:32:55 +1000

    placeholders for future type treatments

--- server/src/extractors/pdfminer/pdfminer.ts
+++ server/src/extractors/pdfminer/pdfminer.ts
@@ -113,7 +113,33 @@ function getPage(pageObj: PdfminerPage): Page {
 	});
 	elements = [...elements, ...paras];
 
-	// todo: treat figures, rects, curves, layout... before creating the page
+	// // treat figure
+	// if (Object.keys(pageObj).includes('figure')) {
+	// 	logger.info(
+	// 		`Here's a figure on page ${pageObj._attr.id}: ${utils.prettifyObject(pageObj.figure)}`,
+	// 	);
+	// }
+
+	// // treat curve
+	// if (Object.keys(pageObj).includes('curve')) {
+	// 	logger.info(
+	// 		`Here's a curve on page ${pageObj._attr.id}: ${utils.prettifyObject(pageObj.curve)}`,
+	// 	);
+	// }
+
+	// // treat rect
+	// if (Object.keys(pageObj).includes('rect')) {
+	// 	logger.info(
+	// 		`Here's a rect on page ${pageObj._attr.id}: ${utils.prettifyObject(pageObj.rect)}`,
+	// 	);
+	// }
+
+	// // treat line
+	// if (Object.keys(pageObj).includes('line')) {
+	// 	logger.info(
+	// 		`Here's a line on page ${pageObj._attr.id}: ${utils.prettifyObject(pageObj.line)}`,
+	// 	);
+	// }
 
 	return new Page(parseFloat(pageObj._attr.id), paras, pageBbox);
 }
