5e711a85546199e9e433b163c2df65fa3e19d8c8
Aarohi Johal <aarohi.johal@gmail.com>
2019-09-02 16:18:05 +1000

    table detection test <-- linter

--- test/table-detection.spec.ts
+++ test/table-detection.spec.ts
@@ -1,11 +1,11 @@
 import { expect } from 'chai';
-import 'mocha';
-import { TableDetectionModule, TableExtractor, TableExtractorResult, Options } from '../server/src/modules/TableDetectionModule';
-import { Element } from '../server/src/types/DocumentRepresentation/Element';
-import { getPdf, runModules } from './helpers';
-import { Table } from '../server/src/types/DocumentRepresentation/Table';
 import * as fs from 'fs';
+import 'mocha';
+import { Options, TableDetectionModule, TableExtractor, TableExtractorResult } from '../server/src/modules/TableDetectionModule';
 import { TableRow } from '../server/src/types/DocumentRepresentation';
+import { Element } from '../server/src/types/DocumentRepresentation/Element';
+import { Table } from '../server/src/types/DocumentRepresentation/Table';
+import { getPdf, runModules } from './helpers';
 
 const pdfName = 'table-detection.pdf';
 
@@ -14,28 +14,28 @@ class TableExtractorStub implements TableExtractor {
     private stderr: string;
     private stdout: string;
 
-    constructor(status: number = 0, stderr: string = "", stdout: string = "[]"){
+    constructor(status: number = 0, stderr: string = "", stdout: string = "[]") {
         this.status = status;
         this.stderr = stderr;
         this.stdout = stdout;
     }
 
-    readTables(_: string, _options: Options): TableExtractorResult {		
+    public readTables(_: string, _options: Options): TableExtractorResult {
         return {
             stdout: this.stdout,
             stderr: this.stderr,
-            status: this.status
+            status: this.status,
         };
-    }    
+    }
 }
 
 describe('No Table detection function', () => {
-    let table: Element;	    
+    let table: Element;
     const noTableDetectedExtractor = new TableExtractorStub(0, "", "[]");
 
-	before(done => {
+	   before(done => {
         getPdf(d => runModules(d, [new TableDetectionModule(null, noTableDetectedExtractor)]), pdfName)
-        .then(([_, pdfAfter]) => {            			
+        .then(([_, pdfAfter]) => {
             pdfAfter.pages[0].getElementsOfType<Table>(Table).forEach(elt => {
                 table = elt;
             });
@@ -43,21 +43,21 @@ describe('No Table detection function', () => {
         });
 	});
 
-	it('should have no table detected', () => {
+	   it('should have no table detected', () => {
         expect(table).not.exist;
-	});	
+	});
 });
 
 describe('One Table detection function', () => {
     let table: Table;
     let tableRows: TableRow[];
-    const extractorOutput = fs.readFileSync(`${__dirname}/assets/mocks/table-detection-one-table.json`, 'utf8');    
-    const oneTableDetectedExtractor = new TableExtractorStub(0,"", extractorOutput);
+    const extractorOutput = fs.readFileSync(`${__dirname}/assets/mocks/table-detection-one-table.json`, 'utf8');
+    const oneTableDetectedExtractor = new TableExtractorStub(0, "", extractorOutput);
 
-	before(done => {
+	   before(done => {
         getPdf(d => runModules(d, [new TableDetectionModule(null, oneTableDetectedExtractor)]), pdfName)
-        .then(([_, pdfAfter]) => {            			
-            pdfAfter.pages[0].getElementsOfType<Table>(Table).forEach(elt => {                
+        .then(([_, pdfAfter]) => {
+            pdfAfter.pages[0].getElementsOfType<Table>(Table).forEach(elt => {
                 table = elt;
                 tableRows = table.content;
             });
@@ -65,15 +65,15 @@ describe('One Table detection function', () => {
         });
 	});
 
-	it('should have one table detected', () => {
+	   it('should have one table detected', () => {
         expect(table).exist;
     });
-    
+
     it('should have first row cell with row span', () => {
         expect(tableRows[0].content[0].rowspan).to.equal(2);
-    });	
-    
+    });
+
     it('should have first row last cell with col span', () => {
         expect(tableRows[0].content[4].colspan).to.equal(2);
-	});	
+	});
 });
