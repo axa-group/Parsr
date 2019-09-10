c72c730147e28640730631c619ce8a71b2a0d53c
Jordi Valls <jordi.valls@axa-groupsolutions.com>
2019-09-10 14:56:33 +0200

    [Vue] Created plugin for Vuetify

--- demo/vue-viewer/src/main.js
+++ demo/vue-viewer/src/main.js
@@ -2,13 +2,15 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './vuex/store';
-import Vuetify from 'vuetify'; // Import Vuetify to your project
-import 'vuetify/dist/vuetify.min.css';
+import '@mdi/font/css/materialdesignicons.css'; // Ensure you are using css-loader
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import vuetify from '@/plugins/vuetify.js';
 
 Vue.config.productionTip = false;
-Vue.use(Vuetify);
 
 new Vue({
+	vuetify,
 	router,
 	store,
 	render: h => h(App),
