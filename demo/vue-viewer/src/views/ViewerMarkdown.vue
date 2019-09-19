299d446ea03f03365fd81909a88f5a6f8a6042f2
Pierre-Loic Doulcet <pierre@pld.io>
2019-09-19 12:46:19 +0200

    Remove linter warnings

--- demo/vue-viewer/src/views/ViewerMarkdown.vue
+++ demo/vue-viewer/src/views/ViewerMarkdown.vue
@@ -1,13 +1,9 @@
 <template>
 	<div class="main viewer">
-		<pre class='text'>
-{{documentMarkdown}}			
-		</pre>
+		<pre class='text'>{{ documentMarkdown }}</pre>
 	</div>
 </template>
-
 <script>
-
 import { mapState } from 'vuex';
 
 export default {
@@ -17,12 +13,12 @@ export default {
 		},
 		...mapState({
 			inputFileName: state => state.inputFileName,
-			documentMarkdown: state => state.documentMarkdown
+			documentMarkdown: state => state.documentMarkdown,
 		}),
 	},
 	mounted() {
 		if (!this.documentMarkdownFetched) {
-			this.$store.dispatch('fetchDocumentMarkdown').catch(error => {
+			this.documentMarkdown = this.$store.dispatch('fetchDocumentMarkdown').catch(error => {
 				console.log(error.message);
 			});
 		}
@@ -32,21 +28,19 @@ export default {
 
 <style lang="scss" scoped>
 .viewer {
-
 	background-color: #f9f9fd;
-
 }
 
 .text {
 	margin: 62px;
 	background-color: white;
 	border: 1px solid rgb(204, 204, 231);
-	padding : 16px;
+	padding: 16px;
 	// word wrap
-	white-space: pre-wrap;       /* css-3 */
-	white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
-	white-space: -pre-wrap;      /* Opera 4-6 */
-	white-space: -o-pre-wrap;    /* Opera 7 */
+	white-space: pre-wrap;
+	white-space: -moz-pre-wrap;
+	white-space: -pre-wrap;
+	white-space: -o-pre-wrap;
 	word-wrap: break-word;  
 	text-align: left;
 }
