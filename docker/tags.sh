0a15f85f8c6552967006f6e365aed63a16384625
sebastien.lallemand@axa.com <2827759+slallemand@users.noreply.github.com>
2019-09-27 12:29:25 +0200

    docker improvements

---
+++ docker/tags.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+case ${DRONE_BRANCH} in
+  master)
+    echo "Add \"stable\" tag"
+    echo -n stable > .tags
+    ;;
+  develop)
+    echo "Add \"latest,develop\" tag"
+    echo -n lastest,develop > .tags
+    ;;
+  
+esac
