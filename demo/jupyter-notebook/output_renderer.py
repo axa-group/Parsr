47f25b6452c8f476e6bff5ca988c4170cc6137bc
Aarohi Johal <aarohi.johal@gmail.com>
2019-11-25 10:56:25 +1100

    [Jupyter] Refactored for an easier/more pythonic access

--- demo/jupyter-notebook/output_renderer.py
+++ demo/jupyter-notebook/output_renderer.py
@@ -1,3 +1,19 @@
+#
+# Copyright 2019 AXA Group Operations S.A.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import uuid
 import json
 from IPython.display import display, Markdown, display_html, display_javascript
@@ -5,10 +21,13 @@ from pygments import highlight
 from pygments.lexers import JsonLexer
 from pygments.formatters import TerminalFormatter
 
-def RenderJSON(dict):
-    json_object = json.loads(json.dumps(dict))
-    json_str = json.dumps(json_object, indent=4, sort_keys=True)
-    print(highlight(json_str, JsonLexer(), TerminalFormatter()))
+class RenderJSON(object):
+    def __init__(self, data):
+        self.render_json(data)
+    def render_json(self, data):
+        json_object = json.loads(json.dumps(data))
+        json_str = json.dumps(json_object, indent=4, sort_keys=True)
+        print(highlight(json_str, JsonLexer(), TerminalFormatter()))
 
 class RenderBigJSON(object):
     def __init__(self, json_data):
