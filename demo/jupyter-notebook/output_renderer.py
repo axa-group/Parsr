773e02e426808e74389fb78dd5978b9d8adf28a2
Aarohi Johal <aarohi.johal@gmail.com>
2019-12-10 14:12:49 +1100

    [Document Versioning] [Python] HTML-table display

--- demo/jupyter-notebook/output_renderer.py
+++ demo/jupyter-notebook/output_renderer.py
@@ -21,27 +21,35 @@ from pygments import highlight
 from pygments.lexers import JsonLexer
 from pygments.formatters import TerminalFormatter
 
+
 class RenderJSON(object):
-    def __init__(self, data):
-        self.render_json(data)
-    def render_json(self, data):
-        json_object = json.loads(json.dumps(data))
-        json_str = json.dumps(json_object, indent=4, sort_keys=True)
-        print(highlight(json_str, JsonLexer(), TerminalFormatter()))
+	def __init__(self, data):
+		self.render_json(data)
+	def render_json(self, data):
+		json_object = json.loads(json.dumps(data))
+		json_str = json.dumps(json_object, indent=4, sort_keys=True)
+		print(highlight(json_str, JsonLexer(), TerminalFormatter()))
 
 
 class RenderMarkdown(object):
-    def __init__(self, markdown_data):
-        self.markdown_data = markdown_data
-        self.uuid = str(uuid.uuid4())
+	def __init__(self, markdown_data):
+		self.markdown_data = markdown_data
+		self.uuid = str(uuid.uuid4())
 
-    def _ipython_display_(self):
-        display(Markdown(self.markdown_data))
+	def _ipython_display_(self):
+		display(Markdown(self.markdown_data))
+
 
 class RenderHTML(object):
-    def __init__(self, html_data):
-        self.html_data = html_data
-        self.uuid = str(uuid.uuid4())
+	def __init__(self, html_data=None, html_file=None):
+		if not html_data and not html_file:
+			print("You need to provide either a filename or raw HTML data for something to be rendered")
+		self.html_data = html_data
+		self.html_file = html_file
+		self.uuid = str(uuid.uuid4())
 
-    def _ipython_display_(self):
-        display(HTML(self.html_data))
+	def _ipython_display_(self):
+		if self.html_data is not None:
+			display(HTML(self.html_data))
+		if self.html_file is not None:
+			HTML(filename=self.filename)
