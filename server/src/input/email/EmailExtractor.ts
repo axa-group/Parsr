9e2e0d17a44b5122d9557ce019885cd9d426397e
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2020-01-02 17:46:08 +0100

    [feature/email-attachments] - joins all relevant attached PDF/images into one single PDF and processes the entire document

--- server/src/input/email/EmailExtractor.ts
+++ server/src/input/email/EmailExtractor.ts
@@ -19,7 +19,22 @@ import { readFileSync, writeFileSync } from 'fs';
 import { simpleParser } from 'mailparser';
 import { Document } from '../../types/DocumentRepresentation';
 import { Extractor } from '../Extractor';
-import { getPdfExtractor } from './../../utils';
+import { CommandExecuter, getPdfExtractor, getTemporaryFile, mergePDFs } from './../../utils';
+
+interface MailAttachmentData {
+  type: string;
+  contentType: string;
+  partId: string;
+  release: string;
+  content: Buffer;
+  contentDisposition: string;
+  filename: string;
+  contentId: string;
+  cid: string;
+  headers: object;
+  checksum: string;
+  size: number;
+}
 
 export class EmailExtractor extends Extractor {
   public async run(inputFile: string): Promise<Document> {
@@ -44,7 +59,7 @@ export class EmailExtractor extends Extractor {
       const data = readFileSync(inputFile);
       const raw = await simpleParser(data);
 
-      const pdfFile = inputFile.replace('.eml', '.pdf');
+      const mainPDF = getTemporaryFile('.pdf');
       const toPDF = new HTMLToPDF(
         (raw.html || '').concat(styles),
         {
@@ -65,11 +80,59 @@ export class EmailExtractor extends Extractor {
       );
 
       const pdfBuffer: Buffer = await toPDF.convert();
-      writeFileSync(pdfFile, pdfBuffer);
-      return getPdfExtractor(this.config).run(pdfFile);
+      writeFileSync(mainPDF, pdfBuffer);
 
+      const pdfFilesToJoin: Array<Promise<string>> = [
+        mainPDF,
+        ...(raw.attachments || []).map(this.attachmentToPDF.bind(this)(raw.html)),
+      ].filter(f => !!f);
+
+      const files = await Promise.all(pdfFilesToJoin);
+      const fullPDF = inputFile.replace('.eml', '.pdf');
+      await mergePDFs(files, fullPDF);
+      const mainDocument: Document = await getPdfExtractor(this.config).run(fullPDF);
+      mainDocument.inputFile = fullPDF;
+      return mainDocument;
     } catch (e) {
       throw e;
     }
+  }
+
+  private attachmentToPDF(rawHTML: string): (data: MailAttachmentData) => Promise<string> {
+    return async (attachment: MailAttachmentData): Promise<string> => {
+      const outputFilePath = getTemporaryFile('.pdf');
+
+      if (attachment.contentType === 'application/pdf') {
+        writeFileSync(outputFilePath, attachment.content);
+        return outputFilePath;
+      }
+
+      if (attachment.contentType.startsWith('image/')) {
+        /*
+          if the attached image is represented in the HTML body as a base64-encoded img
+          then it's not considered as an attached *extra* file
+          (e.g., logo images, social media link images, etc)
+        */
+        const imageSrcRegexp = new RegExp(/src="data:image\/[a-z]{3,4};base64,([a-zA-Z0-9/+=]+)/gm);
+        const imagesInHTML = rawHTML.match(imageSrcRegexp);
+        const fileBase64 = attachment.content.toString('base64');
+        if (imagesInHTML && imagesInHTML.some(i => i.split(',')[1] === fileBase64)) {
+          return null;
+        }
+
+        const imageFile = getTemporaryFile('.' + attachment.filename.split('.')[1]);
+        writeFileSync(imageFile, attachment.content);
+        return CommandExecuter.run(CommandExecuter.COMMANDS.CONVERT, [
+          imageFile,
+          '-units', 'PixelsPerInch',
+          '-density', '96',
+          outputFilePath,
+        ]).then(() => {
+          return outputFilePath;
+        });
+      }
+
+      return null;
+    };
   }
 }
