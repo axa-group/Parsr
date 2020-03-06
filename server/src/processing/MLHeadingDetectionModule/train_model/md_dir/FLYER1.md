9a3ea63e49e3208fb33326eb4f1828451e34087d
mndoye <ndoyemohamed10@gmail.com>
2020-03-06 13:54:10 +0100

    deleted files and added README

--- server/src/processing/MLHeadingDetectionModule/train_model/md_dir/FLYER1.md
+++
@@ -1,9 +0,0 @@
-TURN YOUR INSTRUCTURED DOCUMENTS INTO CLEAN **STRUCTURED DATA**
-
-# PARSR
-
-Free | Open source | Available in beta
-
-Join the community
-[blabla@instagram.com](mailto:blabla@instagram.com)
-@GIT.adresseParsr.com
--- server/src/processing/MLHeadingDetectionModule/train_model/model.js
+++ server/src/processing/MLHeadingDetectionModule/train_model/model.js
@@ -15,19 +15,19 @@ export var DecisionTreeClassifier = function() {
             if (features[1] <= 0.5) {
                 if (features[3] <= 0.5) {
                     if (features[2] <= 0.5) {
-                        classes[0] = 2605; 
-                        classes[1] = 80; 
+                        classes[0] = 2052; 
+                        classes[1] = 72; 
                     } else {
-                        classes[0] = 9288; 
-                        classes[1] = 127; 
+                        classes[0] = 7446; 
+                        classes[1] = 103; 
                     }
                 } else {
                     if (features[2] <= 0.5) {
-                        classes[0] = 261; 
-                        classes[1] = 16; 
+                        classes[0] = 202; 
+                        classes[1] = 17; 
                     } else {
-                        classes[0] = 1408; 
-                        classes[1] = 1170; 
+                        classes[0] = 1132; 
+                        classes[1] = 1001; 
                     }
                 }
             } else {
@@ -36,16 +36,16 @@ export var DecisionTreeClassifier = function() {
                         classes[0] = 2; 
                         classes[1] = 0; 
                     } else {
-                        classes[0] = 106; 
-                        classes[1] = 1299; 
+                        classes[0] = 90; 
+                        classes[1] = 1011; 
                     }
                 } else {
                     if (features[2] <= 0.5) {
-                        classes[0] = 3; 
+                        classes[0] = 2; 
                         classes[1] = 0; 
                     } else {
-                        classes[0] = 311; 
-                        classes[1] = 1944; 
+                        classes[0] = 250; 
+                        classes[1] = 1504; 
                     }
                 }
             }
@@ -53,37 +53,32 @@ export var DecisionTreeClassifier = function() {
             if (features[2] <= 0.5) {
                 if (features[1] <= 0.5) {
                     if (features[3] <= 0.5) {
-                        classes[0] = 66; 
-                        classes[1] = 45; 
+                        classes[0] = 54; 
+                        classes[1] = 36; 
                     } else {
-                        classes[0] = 60; 
-                        classes[1] = 34; 
+                        classes[0] = 48; 
+                        classes[1] = 25; 
                     }
                 } else {
-                    if (features[3] <= 0.5) {
-                        classes[0] = 1; 
-                        classes[1] = 21; 
-                    } else {
-                        classes[0] = 13; 
-                        classes[1] = 48; 
-                    }
+                    classes[0] = 10; 
+                    classes[1] = 22; 
                 }
             } else {
                 if (features[1] <= 0.5) {
                     if (features[3] <= 0.5) {
-                        classes[0] = 136; 
-                        classes[1] = 2118; 
+                        classes[0] = 105; 
+                        classes[1] = 1881; 
                     } else {
-                        classes[0] = 113; 
-                        classes[1] = 2573; 
+                        classes[0] = 91; 
+                        classes[1] = 2106; 
                     }
                 } else {
                     if (features[3] <= 0.5) {
-                        classes[0] = 66; 
-                        classes[1] = 1332; 
+                        classes[0] = 49; 
+                        classes[1] = 1006; 
                     } else {
-                        classes[0] = 42; 
-                        classes[1] = 3674; 
+                        classes[0] = 35; 
+                        classes[1] = 2784; 
                     }
                 }
             }
