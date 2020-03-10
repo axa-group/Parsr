d634e1d0f5efaf2ab8a22a63ebc6aa9ce7c59a17
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2020-03-10 12:45:06 +0100

    copyright heading update + typo

--- server/src/Orchestrator.ts
+++ server/src/Orchestrator.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 AXA Group Operations S.A.
+ * Copyright 2020 AXA Group Operations S.A.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
