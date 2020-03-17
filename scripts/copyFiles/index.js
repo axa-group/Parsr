ffd1c3c0838ca095ed9c7453ca47eeef2c05d1a4
Jordi Valls <jordi.valls@axa-groupsolutions.com>
2020-03-17 16:01:20 +0100

    [Vulnerabilities Fix] Check destination folder before copy files

--- scripts/copyFiles/index.js
+++ scripts/copyFiles/index.js
@@ -1,4 +1,7 @@
 var shell = require('shelljs');
 
+if (!shell.test('-d', './dist/assets')) {
+  shell.mkdir('./dist/assets');
+}
 shell.cp('-u', './server/assets/*.py', './dist/assets/');
 shell.cp('-u', './server/assets/pdf.worker.js', './node_modules/pdfjs-dist/build/');
