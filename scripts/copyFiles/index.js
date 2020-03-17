b6c928c4b12da75ef78fdabe4734330f1aedad10
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2020-03-17 17:18:39 +0100

    [Vulnerabilities Fix] - mkdir fix

--- scripts/copyFiles/index.js
+++ scripts/copyFiles/index.js
@@ -1,7 +1,7 @@
 var shell = require('shelljs');
 
 if (!shell.test('-d', './dist/assets')) {
-  shell.mkdir('./dist/assets');
+  shell.mkdir('./dist', './dist/assets');
 }
 shell.cp('-u', './server/assets/*.py', './dist/assets/');
 shell.cp('-u', './server/assets/pdf.worker.js', './node_modules/pdfjs-dist/build/');
