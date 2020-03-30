f779eda8f771643a12c8353b5e9e8a2aa1532592
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2020-03-30 14:07:11 +0200

    fix for default config file

--- server/bin/index.ts
+++ server/bin/index.ts
@@ -49,7 +49,7 @@ function main(): void {
     .option(
       '-c, --config <filename>',
       "The file's path from which the application's parameters will be loaded",
-      `${__dirname}/defaultConfig.json`
+      `${__dirname}/defaultConfig.json`,
     )
     .option(
       '-l, --log-level <verbosity>',
