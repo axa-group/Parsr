b40fdf3efd137d3bff40d8f655c593832ac7ff68
yuran <yuran@iMac-de-yuran.local>
2020-04-06 12:34:43 +0200

    add upgrade brew python and run python3

--- scripts/preinstall/darwin.bash.sh
+++ scripts/preinstall/darwin.bash.sh
@@ -4,7 +4,8 @@ tmp_dir=$(mktemp -d -t parsr-install)
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" &&
 brew install qpdf imagemagick tesseract tesseract-lang tcl-tk ghostscript mupdf-tools pandoc &&
+brew upgrade python &&
 curl https://bootstrap.pypa.io/get-pip.py -o $tmp_dir/get-pip.py &&
-python $tmp_dir/get-pip.py &&
+python3 $tmp_dir/get-pip.py &&
 pip install pdfminer.six camelot-py[cv] PyPDF2 &&
 rm -rf $tmp_dir
