96914ad23dfa6e539a30944e4e89a284ff12c1d3
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2020-04-08 12:07:43 +0200

    [macOS install script] - made a few required changes
    - brew: install last python version
    - pip: install pip on python and python3

--- scripts/preinstall/darwin.bash.sh
+++ scripts/preinstall/darwin.bash.sh
@@ -3,10 +3,10 @@
 tmp_dir=$(mktemp -d -t parsr-install) 
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" &&
-brew install qpdf imagemagick tesseract tesseract-lang tcl-tk ghostscript mupdf-tools pandoc &&
-brew upgrade python &&
+brew install python qpdf imagemagick tesseract tesseract-lang tcl-tk ghostscript mupdf-tools pandoc &&
 curl https://bootstrap.pypa.io/get-pip.py -o $tmp_dir/get-pip.py &&
 python3 $tmp_dir/get-pip.py &&
+python $tmp_dir/get-pip.py &&
 pip3 install pdfminer.six camelot-py[cv] &&
-python2.7 -m pip install PyPDF2 &&
+python -m pip install PyPDF2 &&
 rm -rf $tmp_dir
