1a7ebc5eff3171fcf7c1f90f4d49fbfb91b1dc3d
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2020-04-17 10:34:52 +0200

    [pdfjs] - no more need to patch pdfjs worker

--- scripts/copyFiles/index.js
+++ scripts/copyFiles/index.js
@@ -20,5 +20,4 @@ if (!shell.test('-d', './dist/assets') || !shell.test('-d', './dist/bin')) {
   shell.mkdir('./dist', './dist/assets', './dist/bin');
 }
 shell.cp('-u', './server/assets/*.py', './dist/assets/');
-shell.cp('-u', './server/assets/pdf.worker.js', './node_modules/pdfjs-dist/build/');
 shell.cp('-u', './server/defaultConfig.json', './dist/bin/');
