5e6cc4ab50faa6786d58d01b2654cedd8a395802
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2020-04-29 17:46:44 +0200

    [svg-shapes] - render Drawing elements on UI

---
+++ demo/vue-viewer/src/components/DocumentPreview/SvgShape.vue
@@ -0,0 +1,14 @@
+<template functional>
+  <svg>
+    <line
+      v-for="svgLine in props.element.content"
+      :key="svgLine.id"
+      :id="'SVG_' + svgLine.id"
+      :x1="svgLine.fromX"
+      :y1="svgLine.fromY"
+      :x2="svgLine.toX"
+      :y2="svgLine.toY"
+      :style="`stroke: ${svgLine.color}; stroke-width: ${svgLine.thickness}; opacity: 0.1`"
+    />
+  </svg>
+</template>
