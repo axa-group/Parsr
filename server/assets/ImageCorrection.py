5e60d7f6b85e505968cae9fd82791c13b384229c
David Felix <david_felix@live.fr>
2020-05-04 11:02:51 +0200

    [clean divers files] restore openCV functions names

--- server/assets/ImageCorrection.py
+++ server/assets/ImageCorrection.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 This script performs all (TODO Deskewing) steps to improve Tesseract accuracy:
-See --> https://github.com/tesseract-ocr/tesseract/wiki/_improve_quality
+See --> https://github.com/tesseract-ocr/tesseract/wiki/ImproveQuality
 """
 from __future__ import print_function
 
@@ -35,7 +35,7 @@ def remove_shadow(image):
     result_planes = []
     for plane in rgb_planes:
         dilated_img = cv.dilate(plane, np.ones((5,5), np.uint8))
-        bg_img = cv.median_blur(dilated_img, 11)
+        bg_img = cv.medianBlur(dilated_img, 11)
         diff_img = 255 - cv.absdiff(plane, bg_img)
         result_planes.append(diff_img)
 
@@ -43,13 +43,13 @@ def remove_shadow(image):
 
 def detect_rotation(image):
     #if len(image.shape) >= 3:
-    img_gray = cv.cvt_color(image, cv.COLOR_BGR2YUV)
+    img_gray = cv.cvtColor(image, cv.COLOR_BGR2YUV)
     image_y = np.zeros(img_gray.shape[0:2], np.uint8)
     image_y[:, :] = img_gray[:, :, 0]
 
-    image_blurred = cv._gaussian_blur(image_y, (3, 3), 0)
+    image_blurred = cv.GaussianBlur(image_y, (3, 3), 0)
     img_edges = cv.Canny(image_blurred, 250, 250, aperture_size=3)
-    lines = cv._hough_lines_p(img_edges, 1, math.pi / 180.0, 100, min_line_length=100, max_line_gap=20)
+    lines = cv.HoughLinesP(img_edges, 1, math.pi / 180.0, 100, min_line_length=100, max_line_gap=20)
 
     test_image = image.copy()
     angles = []
@@ -68,7 +68,7 @@ def rotate_image(mat, angle):
   height, width = mat.shape[:2]
   image_center = (width/2, height/2)
 
-  rotation_mat = cv.get_rotation_matrix2_d(image_center, angle, 1.)
+  rotation_mat = cv.getRotationMatrix2D(image_center, angle, 1.)
 
   abs_cos = abs(rotation_mat[0,0])
   abs_sin = abs(rotation_mat[0,1])
@@ -79,7 +79,7 @@ def rotate_image(mat, angle):
   rotation_mat[0, 2] += bound_w/2 - image_center[0]
   rotation_mat[1, 2] += bound_h/2 - image_center[1]
 
-  rotated_mat = cv.warp_affine(mat, rotation_mat, (bound_w, bound_h), border_value=(255,255,255))
+  rotated_mat = cv.warpAffine(mat, rotation_mat, (bound_w, bound_h), border_value=(255,255,255))
   return rotated_mat
 
 def get_rotation_data(original_image, rotated_image, angle, output_file):
@@ -128,7 +128,7 @@ def main():
             rotated_image = rotate_image(no_transparent_image, angle)
         # Remove shadows
         shadows_out = remove_shadow(rotated_image)
-        shadows_out = cv.copy_make_border(shadows_out, 2, 2, 2, 2, cv.BORDER_CONSTANT, value=[1, 0, 0])
+        shadows_out = cv.copyMakeBorder(shadows_out, 2, 2, 2, 2, cv.BORDER_CONSTANT, value=[1, 0, 0])
 
         #save image
         output_file_name = src.split('.')[0]+'-corrected.'+'.'.join(src.split('.')[1:])
