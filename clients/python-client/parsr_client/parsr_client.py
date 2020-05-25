e330f5e1c7639c0bcd7674c5228687308e58dc97
Aarohi Johal <aarohi.johal@gmail.com>
2020-05-25 15:36:46 +1000

    Documented functions too

--- clients/python-client/parsr_client/parsr_client.py
+++ clients/python-client/parsr_client/parsr_client.py
@@ -38,18 +38,36 @@ class ParsrClient():
     send and receive data to/from the Parsr server using the API.
     """
 
-    def __init__(self, server, revision_history: dict = {}):
-        self.revision_history = revision_history
+    def __init__(self, server: str, revision_history: dict = {}):
+        """Constructor for the class.
+
+        - server: The address of the server. For example: 'localhost:3001'
+        - revision_history: The revision history of a client, if a previous
+        state is to be restored.
+        """
         self.set_server(server)
+        self.set_revision_history(revision_history)
         self.set_current_request_id("")
 
     def __supported_input_files(self) -> list:
-        return ['*.pdf', '*.jpg', '*.jpeg', '*.png', '*.tiff', '*.tif']
+        """Returns the file extensions supported as input files for Parsr
+        """
+        return ['*.pdf', '*.jpg', '*.jpeg', '*.png', '*.tiff',
+                '*.tif', '*.docx', '*.xml', '*.eml' '*.json']
 
     def set_server(self, server: str):
+        """Setter for the Parsr server's address
+        """
         self.server = server
 
+    def set_revision_history(self, revision_history: dict):
+        """Set a previous revision history for the client
+        """
+        self.revision_history = revision_history
+
     def set_current_request_id(self, request_id: str):
+        """Set the current request_id for the client
+        """
         self.request_id = request_id
 
     def send_document(
@@ -60,9 +78,22 @@ class ParsrClient():
             document_name: str = None,
             revision: str = 'major',
             wait_till_finished: bool = False,
-            refresh_period=2,
+            refresh_period: int = 2,
             save_request_id: bool = False,
             silent: bool = True) -> dict:
+        """Send the document to the Parsr server
+
+        - file: The address of the file to be sent to the server
+        - config: The configuration file to be sent with the request
+        - document_name: The name of the document
+        - revision: Type of revision (major or minor) of the document
+        - wait_till_finished: Should the sending be blocking or not
+        - refresh_period: How often the server is to be polled to see
+        if the job has finished
+        - save_request_id: If set to true, will remember the current
+        job and it won't have to be specified again
+        - silent: If set to false, each polling request will be logged.
+        """
         if server == "":
             if self.server == "":
                 raise Exception('No server address provided')
@@ -117,6 +148,11 @@ class ParsrClient():
                 'server_response': r.text}
 
     def get_request_id(self, document_name: str, revision: str) -> str:
+        """Gets the request ID from a document name and a revision ID
+
+        - document_name: The name of the document
+        - revision: The ID of the revision being queried
+        """
         if document_name in self.revision_history:
             if revision in self.revision_history[document_name].keys():
                 return self.revision_history[document_name][revision]
@@ -129,15 +165,25 @@ class ParsrClient():
         return ""
 
     def get_revisions(self, document_name: str) -> list:
+        """Get a list of all the revisions given a document name
+
+        - document_name: The name of the document
+        """
         if document_name in self.revision_history:
             return list(self.revision_history[document_name].keys())
         else:
             return []
 
     def get_document_name_from_request_id(self, request_id: str) -> str:
+        """Get the name of a document knowing only its request ID
+
+        - request_id: the request id for which the document is to be searched
+        """
         for document_name in list(self.revision_history.keys()):
             if request_id in [self.revision_history[document_name][i]
-                              for i in list(self.revision_history[document_name])]:
+                              for i in list(
+                                      self.revision_history[document_name]
+                              )]:
                 return document_name
         return ""
 
@@ -146,6 +192,15 @@ class ParsrClient():
             document_name: str,
             revisions: list = [],
             pretty_html: bool = False) -> list:
+        """Compare two documents and return the diff between them
+
+        - document_name: the name of the document
+        - revisions: the list of revisions to be taken into consideration.
+        When nothing is supplied, all the revisions of the document are
+        compared
+        - pretty_html: if a general diff is to be returned or should a pretty
+        html document is to be generated instead
+        """
         diffs = []
         if len(revisions) == 0:
             revisions = self.get_revisions(document_name)
@@ -177,6 +232,12 @@ class ParsrClient():
             folder: str,
             config: str,
             server: str = "") -> list:
+        """Send all the files inside a folder
+
+        - folder: The name of the folder to be sent to Parsr
+        - config: The address to the configuration file to be used
+        for treating the documents
+        """
         if server == "":
             if self.server == "":
                 raise Exception('No server address provided')
@@ -184,7 +245,7 @@ class ParsrClient():
                 server = self.server
                 responses = []
                 chdir(folder)
-                files = [glob.glob(e) for e in self.__supported_input_files()]
+                files = [glob(e) for e in self.__supported_input_files()]
                 files_flat = list(chain.from_iterable(files))
         for file in files_flat:
             packet = {
@@ -203,6 +264,11 @@ class ParsrClient():
         return responses
 
     def get_status(self, request_id: str = "", server: str = ""):
+        """Get the status of a partcular request using its ID
+
+        - request_id: The ID of the request to be queried with the server
+        - server: The server address where the query is to be made
+        """
         if server == "":
             if self.server == "":
                 raise Exception('No server address provided')
@@ -223,6 +289,12 @@ class ParsrClient():
                 r.text)}
 
     def get_json(self, request_id: str = "", server: str = ""):
+        """Fetch the Parsr's output JSON file (result) given a particular
+        request
+
+        - request_id: The ID of the request to be queried with the server
+        - server: The server from which the JSON is to be fetched
+        """
         if server == "":
             if self.server == "":
                 raise Exception('No server address provided')
@@ -241,6 +313,12 @@ class ParsrClient():
             return {'request_id': request_id, 'server_response': r.json()}
 
     def get_markdown(self, request_id: str = "", server: str = ""):
+        """Fetch the Parsr's output Markdown file (result) given a particular
+        request
+
+        - request_id: The ID of the request to be queried with the server
+        - server: The server from which the result is to be fetched
+        """
         if server == "":
             if self.server == "":
                 raise Exception('No server address provided')
@@ -259,6 +337,12 @@ class ParsrClient():
             return {'request_id': request_id, 'server_response': r.text}
 
     def get_text(self, request_id: str = "", server: str = ""):
+        """Fetch the Parsr's output Text file (result) given a particular
+        request
+
+        - request_id: The ID of the request to be queried with the server
+        - server: The server from which the result is to be fetched
+        """
         if server == "":
             if self.server == "":
                 raise Exception('No server address provided')
@@ -277,6 +361,9 @@ class ParsrClient():
             return {'request_id': request_id, 'server_response': r.text}
 
     def get_tables_info(self, request_id: str = ""):
+        """Fetch the list and location of tables detected inside a particular
+        document.
+        """
         return [(table.rsplit('/')[-2], table.rsplit('/')[-1])
                 for table in literal_eval(
                         self.get_table(request_id=request_id).columns[0])]
@@ -289,6 +376,15 @@ class ParsrClient():
             seperator=";",
             server: str = "",
             column_names: list = None):
+        """Get a particular table from a processed document.
+
+        - request_id: The request to be queried to get a document.
+        - page: The page number on which the queried table exists.
+        - table: The table number to be fetched.
+        - seperator: The seperator to be used between table cells (default ';')
+        - server: The server address which is to be queried.
+        - column_names: The headings of the table searched (column titles)
+        """
         if server == "":
             if self.server == "":
                 raise Exception('No server address provided')
